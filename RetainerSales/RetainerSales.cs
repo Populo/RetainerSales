44dffad74d1e322105bc834c496febe5a88fc3dc
dave hotdog <25019718+Populo@users.noreply.github.com>
2024-12-19 10:18:32 -0500

    try/catch mostly

--- RetainerSales/RetainerSales.cs
+++ RetainerSales/RetainerSales.cs
@@ -5,17 +5,9 @@ using Dalamud.Plugin;
 using Dalamud.Game;
 using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
-using Dalamud.Hooking;
 using Dalamud.Plugin.Services;
-using Dalamud.Utility;
 using FFXIVClientStructs.FFXIV.Client.Game;
-using FFXIVClientStructs.FFXIV.Client.Game.Character;
-using FFXIVClientStructs.FFXIV.Client.Graphics.Render;
-using FFXIVClientStructs.FFXIV.Client.Graphics.Scene;
-using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Component.GUI;
-using Lumina.Data.Parsing;
-using Lumina.Excel.Sheets;
 
 namespace RetainerSales;
 
@@ -27,7 +19,7 @@ public sealed unsafe class RetainerSales : IDalamudPlugin
     public IAddonLifecycle AddonLifecycle { get; init; }
     public IClientState ClientState { get; init; }
 
-    private Dictionary<string, int> RetainerSaleNumbers { get; set; } = new ();
+    private Dictionary<string, int> RetainerSaleNumbers { get; set; } = new();
     private Dictionary<string, uint> RetainerIndex { get; set; } = new();
 
     public RetainerSales(
@@ -48,7 +40,7 @@ public sealed unsafe class RetainerSales : IDalamudPlugin
         {
             AddonLifecycle.RegisterListener(AddonEvent.PreSetup, "RetainerList", HandleOpen);
             AddonLifecycle.RegisterListener(AddonEvent.PreDraw, "RetainerList", HandleRefresh);
-            
+
             AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "SelectString", HandleRetainerSelect);
         }
         catch (Exception e)
@@ -66,10 +58,10 @@ public sealed unsafe class RetainerSales : IDalamudPlugin
             for (uint i = 0; i < manager->GetRetainerCount(); ++i)
             {
                 var retainer = manager->GetRetainerBySortedIndex(i);
-                
-                if (!Configuration.ItemsForSale.ContainsKey(retainer->NameString)) 
+
+                if (!Configuration.ItemsForSale.ContainsKey(retainer->NameString))
                     Configuration.ItemsForSale[retainer->NameString] = retainer->MarketItemCount;
-                
+
                 RetainerSaleNumbers[retainer->NameString] = retainer->MarketItemCount;
 
                 if (Configuration.ItemsForSale[retainer->NameString] > retainer->MarketItemCount)
@@ -88,44 +80,58 @@ public sealed unsafe class RetainerSales : IDalamudPlugin
             PluginLog.Fatal(e.Message);
         }
     }
-    
+
     private void HandleRefresh(AddonEvent type, AddonArgs args)
     {
-        var addon = (AtkUnitBase*)args.Addon;
-        var retainerListNode = (AtkComponentNode*)addon->GetNodeById(27);
-
-        foreach (var ret in RetainerIndex)
+        try
         {
-            var retRow = GetNodeByID<AtkComponentNode>(&retainerListNode->Component->UldManager, 
-                                                       ret.Value == 0 ? 4 : 41000 + ret.Value, 
-                                                       (NodeType)1011);
+            var addon = (AtkUnitBase*)args.Addon;
+            var retainerListNode = (AtkComponentNode*)addon->GetNodeById(27);
 
-            var saleColumn = (AtkTextNode*)retRow->Component->UldManager.SearchNodeById(11);
-            var saleText = saleColumn->NodeText.ToString();
-            if (!saleText.Contains("<<<"))
-                saleColumn->SetText($"{saleText} <<<");
+            foreach (var ret in RetainerIndex)
+            {
+                var retRow = GetNodeByID<AtkComponentNode>(&retainerListNode->Component->UldManager,
+                                                           ret.Value == 0 ? 4 : 41000 + ret.Value,
+                                                           (NodeType)1011);
+
+                var saleColumn = (AtkTextNode*)retRow->Component->UldManager.SearchNodeById(11);
+                var saleText = saleColumn->NodeText.ToString();
+                if (!saleText.Contains("<<<"))
+                    saleColumn->SetText($"{saleText} <<<");
+            }
+        }
+        catch (Exception e)
+        {
+            PluginLog.Fatal(e.Message);
         }
     }
     
     private void HandleRetainerSelect(AddonEvent type, AddonArgs args)
     {
-        var addon = (AtkUnitBase*)args.Addon;
-        var titleTextNode = (AtkTextNode*)addon->GetNodeById(2);
-
-        var titleText = titleTextNode->NodeText.ToString();
-        var retName = ClientState.ClientLanguage switch
+        try
         {
-            ClientLanguage.Japanese => titleText.Split('\r').Last().Replace("）", "").Replace("（", ""),
-            ClientLanguage.German => titleText.Split("Du hast ").Last().Split(" herbeigerufen").First(),
-            ClientLanguage.French => titleText.Split("Menu de ").Last().Split(" [").First(),
-            ClientLanguage.English => titleText.Split('\r').First().Split(": ").Last(),
-            _ => ""
-        };
-        PluginLog.Verbose(retName);
-        
-        RetainerIndex.Remove(retName);
-        Configuration.ItemsForSale[retName] = RetainerSaleNumbers[retName];
-        Configuration.Save();
+            var addon = (AtkUnitBase*)args.Addon;
+            var titleTextNode = (AtkTextNode*)addon->GetNodeById(2);
+
+            var titleText = titleTextNode->NodeText.ToString();
+            var retName = ClientState.ClientLanguage switch
+            {
+                ClientLanguage.Japanese => titleText.Split('\r').Last().Replace("）", "").Replace("（", ""),
+                ClientLanguage.German => titleText.Split("Du hast ").Last().Split(" herbeigerufen").First(),
+                ClientLanguage.French => titleText.Split("Menu de ").Last().Split(" [").First(),
+                ClientLanguage.English => titleText.Split('\r').First().Split(": ").Last(),
+                _ => ""
+            };
+            PluginLog.Verbose(retName);
+
+            RetainerIndex.Remove(retName);
+            Configuration.ItemsForSale[retName] = RetainerSaleNumbers[retName];
+            Configuration.Save();
+        }
+        catch (Exception e)
+        {
+            PluginLog.Fatal(e.Message);
+        }
     }
     
     // ripped from simple tweaks (https://github.com/Caraxi/SimpleTweaksPlugin/blob/7758e07bd836f7d4ceb78ee87a6723170cc0b5fb/Utility/Common.cs#L269)
